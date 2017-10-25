ed53550699a67f02761dc870126711ba7cc672bd
Stuart Moir <stuart.a.moir@live.com>
2017-10-25 11:40:28 +0100

    Initial commit; imports data structure to React from Firebase

---
+++ my-app/src/App.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import fire from './firebase.js'
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+
+  constructor(props){
+    super(props)
+    this.state = {
+      data: {}
+    }
+  }
+
+  componentDidMount(){ 
+   const data = fire.database().ref('/')
+    data.on('value', (snapshot) => {
+      const data = snapshot.val()
+      this.setState({
+        data: data
+      })
+    })
+  }
+
+  render() {
+
+    console.log(this.state.data)
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ my-app/src/firebase.js
@@ -0,0 +1,13 @@
+import firebase from 'firebase'
+
+var config = {
+  apiKey: "AIzaSyAI2JNsY61r7v2Z9xh2EJqnSUHL7cFAi2s",
+  authDomain: "smoir-beezer-code-test.firebaseapp.com",
+  databaseURL: "https://smoir-beezer-code-test.firebaseio.com",
+  projectId: "smoir-beezer-code-test",
+  storageBucket: "smoir-beezer-code-test.appspot.com",
+  messagingSenderId: "249648350632"
+}
+var fire = firebase.initializeApp(config)
+
+export default fire
