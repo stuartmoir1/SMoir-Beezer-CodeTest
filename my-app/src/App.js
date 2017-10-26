89d174570011b083582d89229f9b6f429668c446
Stuart Moir <stuart.a.moir@live.com>
2017-10-26 14:48:53 +0100

    Ditto: removes seed data and commented out code from App.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import Table from './components/Table'
 import fire from './other/firebase.js'
 import {dashboardData} from './other/database_data.js'
@@ -37,15 +37,4 @@ class App extends Component {
   }
 }
 
-export default App;
-
-//if (data.length !== 0){
-//  console.log('userID:', data[6]['userId'])
-//  console.log('userName:', data[6]['userName'])
-//  console.log('accID:', data[6]['accId'])
-//  console.log('account details:', data[6]['appDetails'])
-//
-//  const key = Object.keys(data[6]['appDetails'][0])
-//  console.log('title:', key.toString())
-//  console.log('rating:', data[6]['appDetails'][0][key])
-//}
+export default App
