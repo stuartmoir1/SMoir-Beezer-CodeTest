303de25c3a416366cd8592619085f58df19d4473
Stuart Moir <stuart.a.moir@live.com>
2017-10-29 15:56:44 +0000

    Adds tentative start to star-based rating system functionality

--- my-app/src/components/AppDetails.jsx
+++ my-app/src/components/AppDetails.jsx
@@ -4,11 +4,18 @@ function AppDetails(props){
   //console.log('AppDetails...')
   const details = props.details
   const title = (Object.keys(details)).toString()
-  const rating = details[(Object.keys(details)).toString()]
-  const toDisplay = title + '; ' + rating + '/5' 
+  //const rating = details[(Object.keys(details)).toString()]
+  //const toDisplay = title + '; ' + rating + '/5' 
   return (
     <div>
-      {toDisplay}
+      {title}
+      <div className='app-rating'>
+        <span id='star5'>☆</span>
+        <span id='star4'>☆</span>
+        <span id='star3'>☆</span>
+        <span id='star2'>☆</span>
+        <span id='star1'>☆</span>
+      </div>
     </div>
   )
 }
