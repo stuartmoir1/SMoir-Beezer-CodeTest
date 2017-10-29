71f919b08eb58c5740e145ed73a278f1d6206cfd
Stuart Moir <stuart.a.moir@live.com>
2017-10-29 13:54:12 +0000

    Converts component AppDetails.jsx to a function as original implementation as class was stateless

--- my-app/src/components/AppDetails.jsx
+++ my-app/src/components/AppDetails.jsx
@@ -1,19 +1,15 @@
-import React, {Component} from 'react'
+import React from 'react'
 
-class AppDetails extends Component{
-
-  render(){
-    //console.log('AppDetails, render...')
-    const details = this.props.details
-    const title = (Object.keys(details)).toString()
-    const rating = details[(Object.keys(details)).toString()]
-    const toDisplay = title + '; ' + rating + '/5' 
-    return (
-      <div>
-        {toDisplay}
-      </div>
-    )
-  }
+function AppDetails(props){
+  const details = props.details
+  const title = (Object.keys(details)).toString()
+  const rating = details[(Object.keys(details)).toString()]
+  const toDisplay = title + '; ' + rating + '/5' 
+  return (
+    <div>
+      {toDisplay}
+    </div>
+  )
 }
 
 export default AppDetails
