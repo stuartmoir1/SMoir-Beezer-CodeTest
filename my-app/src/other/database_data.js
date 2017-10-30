779bad945bebf04807004918b86b6928172863dd
Stuart Moir <stuart.a.moir@live.com>
2017-10-30 12:24:02 +0000

    Adds minor reordering of variable declarations in database_data.js

--- my-app/src/other/database_data.js
+++ my-app/src/other/database_data.js
@@ -1,23 +1,23 @@
 const dashboardData = (data) => {
 
   //console.log('dashboardData...')
-  const users = data['users']
   const accs = data['accounts']
+  const users = data['users']
   let dashboard = []
 
   for (let user in users){
 
     const userId = user
-    const userName = users[user]['name']
     const accId = users[user]['account']
+    const userName = users[user]['name']
     let appDetails = []
 
     for (let acc in accs){
       if (acc === accId){
         const apps = (Object.keys(accs[acc]['apps']))
         for (let app of apps){
-          const title = accs[acc]['apps'][app]['title']
           const rating = accs[acc]['apps'][app]['rating']
+          const title = accs[acc]['apps'][app]['title']
           appDetails.push({[title]: rating})
         }
         break // Only one account per user.
